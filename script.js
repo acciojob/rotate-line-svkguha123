488ddc8007d34930183a4708c1cb40504e3b292a
Souvik Guha <svkguha123@gmail.com>
2023-04-28 13:41:10 +0530

    up

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const line = document.getElementById("line");
+let angle = 0;
+
+setInterval(() => {
+  angle += 1;
+  line.style.transform = `rotate(${angle}deg)`;
+}, 10);
